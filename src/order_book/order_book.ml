ad5caf4f6893ee9f8f5202c63cdde859de402b05
yannick <yannick@schuermann.org>
2025-05-04 20:26:54 +0200

    added order book ml and mli aswell as dune

---
+++ src/order_book/order_book.ml
@@ -0,0 +1,12 @@
+(* open Types_lib.Types *)
+
+(* let f (id : order_id) (user_id : user_id) (side: side) (price : float option) (quantity : int) (timestamp : float) : order = *)
+(*   { *)
+(*     id = id; *)
+(*     user_id = user_id; *)
+(*     side = side; *)
+(*     price = price; *)
+(*     quantity = quantity; *)
+(*     timestamp = timestamp; *)
+(*   } *)
+  
