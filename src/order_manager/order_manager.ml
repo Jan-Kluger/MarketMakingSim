1b414ace69e1dd0a35deec535c97b94f6ab55c93
yannick <yannick@schuermann.org>
2025-05-07 23:18:42 +0200

    fixed order maneger types

--- src/order_manager/order_manager.ml
+++ src/order_manager/order_manager.ml
@@ -1,32 +1,40 @@
 open Types_lib.Types
 
 module type ORDER_MANAGER = sig
-  val submit_order  : order          -> unit Lwt.t
-  val cancel_order  : user_id:int
-                    -> order_id:int -> unit Lwt.t
-  val get_wallet    : user_id:int   -> (float * float) option Lwt.t
-  val get_order_book: unit          -> order list * order list
+  val submit_order    : order             -> submitAck
+  val cancel_order    : cancelReq         -> cancelAck
+  val get_wallet      : orderId           -> walletAck
+  val order_alive     : orderId           -> aliveAck
+  val get_market_data : marketDataRequest -> marketDataAck                                
 end
 
 module ORDER_MANAGER_impl : ORDER_MANAGER = struct
 
-  let submit_order (order : order) : unit Lwt.t =
+  let submit_order (order : order) : submitAck =
     (* synchronously print to stdout, then return a resolved promise *)
     Printf.printf "🔔 [mock] submit_order: %s\n%!"
       (show_order order);
-    Lwt.return_unit
+    let ack : submitAck =
+      {
+        order_id = order.id;
+        timestamp = order.timestamp;
+        status = "REJECTED";
+        error_code = 0;
+      }
+    in
+    ack
       
-  let cancel_order ~(user_id : int) ~(order_id : int) : unit Lwt.t =
-    let _u = user_id in
-    let _o = order_id in
+  let cancel_order (_req: cancelReq) : cancelAck =
     failwith "todo"
 
-  let get_wallet ~(user_id : int) : (float * float) option Lwt.t =
+  let get_wallet (user_id : orderId) : walletAck =
     let _u = user_id in
     failwith "todo"
 
-  let get_order_book (a : unit) : order list * order list =
-    let _a = a in
+  let order_alive (_user_id : orderId) : aliveAck =
+    failwith "todo"
+
+  let get_market_data (_req : marketDataRequest) : marketDataAck =
     failwith "todo"
 
 end
--- src/order_manager/order_manager.mli
+++ src/order_manager/order_manager.mli
@@ -1,11 +1,11 @@
 open Types_lib.Types
 
 module type ORDER_MANAGER = sig
-    val submit_order  : order          -> unit Lwt.t
-    val cancel_order  : user_id:int
-                      -> order_id:int -> unit Lwt.t
-    val get_wallet    : user_id:int   -> (float * float) option Lwt.t
-    val get_order_book: unit          -> order list * order list
+  val submit_order    : order             -> submitAck
+  val cancel_order    : cancelReq         -> cancelAck
+  val get_wallet      : orderId           -> walletAck
+  val order_alive     : orderId           -> aliveAck
+  val get_market_data : marketDataRequest -> marketDataAck
 end
 
 module ORDER_MANAGER_impl : ORDER_MANAGER
