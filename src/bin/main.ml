83e906c41dbf42d7a2a5b4aed0ed2894ae44f85e
yannick <yannick@schuermann.org>
2025-05-07 12:26:50 +0200

    new main

--- src/bin/main.ml
+++ src/bin/main.ml
@@ -1,1 +1,8 @@
-let () = print_endline "Hello, World!"
+open Order_manager_lib
+open Gateway_lib
+
+module OM = Order_manager.ORDER_MANAGER_impl
+module G = Server.Gateway(OM)
+
+let () =
+  G.start 8080;
