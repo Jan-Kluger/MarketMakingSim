ac15d57811a8ccdf30206fda7751ba1995e7b41a
yannick <yannick@schuermann.org>
2025-05-14 15:22:23 +0200

    expanded order manager

--- src/order_manager/order_manager.ml
+++ src/order_manager/order_manager.ml
@@ -1,15 +1,19 @@
 open Types_lib.Types
+open Wallet_store_lib
 
 module type ORDER_MANAGER = sig
   val submit_order    : order             -> submitAck
   val cancel_order    : cancelReq         -> cancelAck
   val get_wallet      : orderId           -> walletAck
   val order_alive     : orderId           -> aliveAck
-  val get_market_data : marketDataRequest -> marketDataAck                                
+  val get_market_data : marketDataRequest -> marketDataAck
+  val register_user   : userId            -> int option
 end
 
-module ORDER_MANAGER_impl : ORDER_MANAGER = struct
+module ORDER_MANAGER_impl (W : Wallet_store.WALLET_STORE) : ORDER_MANAGER = struct
 
+(* LogBook functions *)
+  
   let submit_order (order : order) : submitAck =
     (* synchronously print to stdout, then return a resolved promise *)
     Printf.printf "🔔 [mock] submit_order: %s\n%!"
@@ -27,14 +31,21 @@ module ORDER_MANAGER_impl : ORDER_MANAGER = struct
   let cancel_order (_req: cancelReq) : cancelAck =
     failwith "todo"
 
-  let get_wallet (user_id : orderId) : walletAck =
-    let _u = user_id in
-    failwith "todo"
-
   let order_alive (_user_id : orderId) : aliveAck =
     failwith "todo"
 
   let get_market_data (_req : marketDataRequest) : marketDataAck =
+    failwith "todo"
+
+  (* Wallet functions *)
+
+  let register_user (_uid : userId) =
+    
+    failwith "todo"
+
+  let get_wallet (user_id : userId) : walletAck =
+    let _u = user_id in
+    let _t = W.get_wallet user_id in
     failwith "todo"
 
 end
--- src/order_manager/order_manager.mli
+++ src/order_manager/order_manager.mli
@@ -1,11 +1,14 @@
 open Types_lib.Types
-
+open Wallet_store_lib
+    
 module type ORDER_MANAGER = sig
   val submit_order    : order             -> submitAck
   val cancel_order    : cancelReq         -> cancelAck
   val get_wallet      : orderId           -> walletAck
   val order_alive     : orderId           -> aliveAck
   val get_market_data : marketDataRequest -> marketDataAck
+  val register_user   : userId            -> int option
+  
 end
 
-module ORDER_MANAGER_impl : ORDER_MANAGER
+module ORDER_MANAGER_impl (_ : Wallet_store.WALLET_STORE) : ORDER_MANAGER
