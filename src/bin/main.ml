e9052deb35add9132553c2e0bee8eea3676b9b9d
yannick <yannick@schuermann.org>
2025-05-16 23:12:50 +0200

    added sqlite 3 dependency and tested main

--- src/bin/main.ml
+++ src/bin/main.ml
@@ -1,7 +1,7 @@
-open Sqlite3
 open Order_manager_lib
 open Gateway_lib
 open Wallet_store_lib
+open Sqlite3
 
 module WS = Wallet_store.WALLET_STORE_impl
 module OM = Order_manager.ORDER_MANAGER_impl(WS)
@@ -11,11 +11,38 @@ let get_db_path () : string =
   let src_dir = Filename.dirname __FILE__ in
   Filename.concat src_dir "/../db/data.db"
 
+let print_table (db : db) (table_name : string) : unit =
+  let sql = Printf.sprintf "SELECT * FROM %s;" table_name in
+  let cb row headers =
+    Array.iteri (fun i cell ->
+      let col = headers.(i) in
+      let v = match cell with Some s -> s | None -> "NULL" in
+      print_endline (col ^ " : " ^ v);
+    ) row;
+    print_endline "";
+    ()
+  in
+  let rc = exec ~cb db sql in
+  if rc <> Rc.OK then
+    failwith ("print_table failed: " ^ Rc.to_string rc)
 
 let () =
   let db = db_open (get_db_path ()) in
+
+  (* Wallet_store.WALLET_STORE_impl.reset_table db; *)
+  (* print_table db "wallets"; *)
   
+  (* let _ = Wallet_store.WALLET_STORE_impl.register_user db "0" in *)
+  (* print_table db "wallets"; *)
+
+  (* let _ = Wallet_store.WALLET_STORE_impl.deposit_funds db "0" 10.0 in *)
+  (* print_table db "wallets"; *)
+
+  (* let _ = Wallet_store.WALLET_STORE_impl.withdraw_funds db "0" 10.0 in *)
+  (* print_table db "wallets"; *)
+
+  let _ = db_close db in
+
   G.start 8080;
   
-  let _ = db_close db in
-  ()
+  () 
