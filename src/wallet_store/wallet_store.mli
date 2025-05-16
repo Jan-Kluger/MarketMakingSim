8547f64bd7f47b50fab0e422727e8e7974a58115
yannick <yannick@schuermann.org>
2025-05-16 23:13:19 +0200

    finished wallet sore sql logic

--- src/wallet_store/wallet_store.mli
+++ src/wallet_store/wallet_store.mli
@@ -1,11 +1,12 @@
 open Types_lib.Types
+open Sqlite3
 
 module type WALLET_STORE = sig
-  val get_wallet : userId -> int option
-
-  val register_user : userId -> int option
-      
-  val deposit_funds : userId -> int option
+  val get_wallet     : db -> userId -> float option
+  val deposit_funds  : db -> userId -> float -> float option
+  val withdraw_funds : db -> userId -> float -> float option
+  val register_user  : db -> userId -> bool
+  val reset_table    : db   -> unit
 end
 
 module WALLET_STORE_impl : WALLET_STORE
